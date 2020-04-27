7532cbd63cbf82347ceefac91837b691cbeade76
fiachi <208473302@qq.com>
2020-04-27 17:53:22 +0800

    GUI计算器

--- src/computer/Computer.java
+++ src/computer/Computer.java
@@ -14,29 +14,71 @@ public class Computer {
     private JButton mButton2;
     private JButton mButton;
     private JButton startButton;
-    private JButton lastButton1;
+    private JButton lastButton;
     private JButton opExpButton;
     private JPanel computer;
     private JTextField inputTextField;
     private JTextField outputTextField;
     private JButton ACButton;
     private JButton mButton3;
-    private JButton finalButton1;
+    private JButton finalButton;
     private JButton nextButton;
 
     public Computer() {
-        opExpButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                show_Result();
-            }
-        });
+        opExpButton.addActionListener(e -> show_Result());
         inputTextField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
                 super.keyReleased(e);
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     show_Result();
+                }
+            }
+        });
+        startButton.addActionListener(e -> {
+            index = start_Index;
+            show_Old(index);
+        });
+        finalButton.addActionListener(e -> {
+            if (length != 0) {
+                index = (start_Index - 1 + length) % length;
+                show_Old(index);
+            }
+        });
+        lastButton.addActionListener(e -> {
+            if (index != start_Index & length != 0) {
+                index = (index - 1 + length) % length;
+            }
+            show_Old(index);
+        });
+        nextButton.addActionListener(e -> {
+            if (length != 0) {
+                if (index != (start_Index - 1 + length) % length) {
+                    index = (index + 1) % length;
+                }
+                show_Old(index);
+            }
+        });
+        cButton.addActionListener(e -> {
+            inputTextField.setText("");
+            outputTextField.setText("");
+        });
+        ACButton.addActionListener(e -> init_All());
+        quitButton.addActionListener(e -> System.exit(0));
+        mButton.addActionListener(e -> outputTextField.setText(String.valueOf(M)));
+        mButton3.addActionListener(e -> {
+            String str = outputTextField.getText();
+            if (!str.equals("")) {
+                if (is_Number(str)) {
+                    M = M + Double.parseDouble(str);
+                }
+            }
+        });
+        mButton2.addActionListener(e -> {
+            String str = outputTextField.getText();
+            if (!str.equals("")) {
+                if (is_Number(str)) {
+                    M = M - Double.parseDouble(str);
                 }
             }
         });
@@ -49,6 +91,36 @@ public class Computer {
         frame.pack();
         frame.setVisible(true);
     }
+    public void init_All() {
+        len = 10;
+        is_Zero_Wrong = false;
+        oldExp = new String[len];
+        oldResult = new String[len];
+        length = 0;
+        index = 0;
+        start_Index = 0;
+        inputTextField.setText("");
+        outputTextField.setText("");
+    }
+    private void add_New() {
+        if (!inputTextField.getText().equals("")) {
+            if (length < len) {
+                oldExp[length] = inputTextField.getText();
+                oldResult[length] = outputTextField.getText();
+                index++;
+                length++;
+            } else {
+                oldExp[start_Index] = inputTextField.getText();
+                oldResult[start_Index] = outputTextField.getText();
+                index = start_Index;
+                start_Index = (start_Index + 1)%len;
+            }
+        }
+    }
+    private void show_Old(int index) {
+        inputTextField.setText(oldExp[index]);
+        outputTextField.setText(oldResult[index]);
+    }
     private void show_Result() {
         String input = inputTextField.getText();
         if (!input.equals("")){
@@ -56,6 +128,7 @@ public class Computer {
                 double output = op_All(input);
                 if (!is_Zero_Wrong) {
                     outputTextField.setText(String.valueOf(output));
+                    add_New();
                 } else {
                     outputTextField.setText("The Divisor can not be Zero");
                     is_Zero_Wrong = false;
@@ -67,13 +140,21 @@ public class Computer {
             outputTextField.setText("No Input!");
         }
     }
+    private int len = 10;
     private boolean is_Zero_Wrong = false;
+    private double M = 0;
+    private String[] oldExp = new String[len];
+    private String[] oldResult = new String[len];
+    private int length = 0;
+    private int index = 0;
+    private int start_Index = 0;
     //private Pattern op_sign = Pattern.compile("[-+*/]{2,}");//运算符重复
     //private Pattern op_start = Pattern.compile("^\\(|(([1-9]\\d+|[0-9])[-+*/].*)");//表达式开头
     //private Pattern op_end = Pattern.compile("(.*[-+*/][1-9]\\d+|[0-9])|\\)$");//表达式结尾
     private Pattern op_mm = Pattern.compile("([1-9]\\d+|[0-9])([-+*/]([1-9]\\d+|[0-9]))*");//无括号表达式匹配
     private Pattern op_kk = Pattern.compile("\\(.*?\\)");//最小括号表达式匹配
-    private Pattern op_as = Pattern.compile("[-+]");//+-
+    //private Pattern op_as = Pattern.compile("[-+]");//+-
+    private Pattern is_num = Pattern.compile("-?[0-9]+(\\.[0-9]+)?");//+-
     private boolean is_Illegal(String input) {
         //按照括号划分，从最小的括号开始
         //如果括号内满足条件，括号内（包括括号）改成1
@@ -100,6 +181,10 @@ public class Computer {
         Matcher m1 = op_mm.matcher(input);
         return !m1.matches();
     }
+    private boolean is_Number(String input) {
+        Matcher m2 = is_num.matcher(input);
+        return m2.matches();
+    }
     private double op_All(String input) {
         Matcher m0 = op_kk.matcher(input);
         String Expmin;
@@ -114,7 +199,7 @@ public class Computer {
         //匹配失败，退出返回false
         return op_Exp(input);
     }
-    private double op_Exp(String input) {//无括号运算
+    private double op_Exp(String input) {//无括号运算，上限200目前
         //先根据加减划分表达式，分成n和n-1两个列表
         //计算每个n的值
         //将每个n的值运算
